84d61da89cb844325862b45979df0919b026ae60
Maximilian Maroe <max.maroe@siliconbeachmedia.com>
2014-10-07 07:51:36 -0700

    Base scripts

---
+++ Assets/07PhysicsFollower/FollowPositionPhysics.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//From: http://answers.unity3d.com/questions/195698/stopping-a-rigidbody-at-target.html
+[RequireComponent(typeof(Rigidbody))]
+public class FollowPositionPhysics : MonoBehaviour
+{
+	public float toVel = 2.5f;
+	public float maxVel = 15.0f;
+	public float maxForce = 40.0f;
+	public float gain = 5f;
+	public Transform target;
+
+	void FixedUpdate()
+	{
+		Vector3 targetPos = target.position;
+		Vector3 dist = targetPos - transform.position;
+		// calc a target vel proportional to distance (clamped to maxVel)
+		Vector3 tgtVel = Vector3.ClampMagnitude(toVel * dist, maxVel);
+		// calculate the velocity error
+		Vector3 error = tgtVel - rigidbody.velocity;
+		// calc a force proportional to the error (clamped to maxForce)
+		Vector3 force = Vector3.ClampMagnitude(gain * error, maxForce);
+		rigidbody.AddForce(force);
+	}
+}
---
+++ Assets/07PhysicsFollower/FollowRotationPhysics.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Based on: http://answers.unity3d.com/questions/48836/determining-the-torque-needed-to-rotate-an-object.html
+//and: http://answers.unity3d.com/questions/195698/stopping-a-rigidbody-at-target.html?sort=oldest
+[RequireComponent(typeof(Rigidbody))]
+public class FollowRotationPhysics : MonoBehaviour
+{
+	public float toVel = 2.5f;
+	public float maxVel = 15.0f;
+	public float maxForce = 40.0f;
+	public float gain = 5f;
+	public Transform target;
+	public bool forceSphericalTensor = false;
+
+	void Start()
+	{
+		if(forceSphericalTensor)
+		{
+		   rigidbody.inertiaTensorRotation = Quaternion.identity;
+		   rigidbody.inertiaTensor = Vector3.one;
+		   rigidbody.centerOfMass = Vector3.zero;
+		}
+	}
+
+	void FixedUpdate()
+	{
+		UpdateAngularVelocity(target.rotation);
+	}
+
+	void UpdateAngularVelocity(Quaternion desired)
+	{
+		//Warning: CopyPasta
+		Vector3 z = Vector3.Cross(transform.forward, desired * Vector3.forward);
+		Vector3 y = Vector3.Cross(transform.up, desired * Vector3.up);
+		
+		float thetaZ = Mathf.Asin(z.magnitude);
+		float thetaY = Mathf.Asin(y.magnitude);
+
+		Vector3 wZ = z.normalized * thetaZ;
+		Vector3 wY = y.normalized * thetaY;
+		Vector3 wZwY = Vector3.ClampMagnitude(toVel * (wZ + wY), maxVel);
+		
+		Quaternion q = transform.rotation * rigidbody.inertiaTensorRotation;
+		Vector3 T = q * Vector3.Scale(rigidbody.inertiaTensor, Quaternion.Inverse(q) * wZwY);
+
+		Vector3 error = T - rigidbody.angularVelocity;
+		Vector3 force = Vector3.ClampMagnitude(gain * error, maxForce);
+
+		rigidbody.AddTorque(force);
+	}
+}
