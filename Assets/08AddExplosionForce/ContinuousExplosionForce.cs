83abe8f9eed5474026facc6a6450485065fd64ae
Maximilian Maroe <max.maroe@siliconbeachmedia.com>
2014-10-08 08:15:00 -0700

    Explosions!

---
+++ Assets/08AddExplosionForce/ContinuousExplosionForce.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ContinuousExplosionForce : MonoBehaviour
+{
+	public float force = 100.0f;
+	public float radius = 5.0f;
+	public float upwardsModifier = 0.0f;
+	public ForceMode forceMode;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		foreach(Collider col in Physics.OverlapSphere(transform.position, radius))
+		{
+			if(col.rigidbody != null)
+			{
+				col.rigidbody.AddExplosionForce(force,transform.position,radius,upwardsModifier,forceMode);
+			}
+		}
+	}
+}
