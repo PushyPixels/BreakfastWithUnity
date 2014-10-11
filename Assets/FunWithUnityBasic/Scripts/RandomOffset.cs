0d614e7f2cb544bcd28ff5dd5f09106b5e6ee90f
Maximilian Maroe <max.maroe@siliconbeachmedia.com>
2014-10-11 15:31:20 -0700

    Safty first

---
+++ Assets/FunWithUnityBasic/Scripts/RandomOffset.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomOffset : MonoBehaviour
+{
+	[System.Serializable]
+	public class MaterialInfo
+	{
+		public string textureName;
+		public Material material;
+	}
+
+	public MaterialInfo[] materialInfoList;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		foreach(MaterialInfo matInfo in materialInfoList)
+		{
+			matInfo.material.SetTextureOffset(matInfo.textureName,new Vector2(Random.value,Random.value));
+		}
+	}
+}
