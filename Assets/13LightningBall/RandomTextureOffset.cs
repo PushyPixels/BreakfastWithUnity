9482193b96dfabcb54f45f33a46e93d83f128733
Maximilian Maroe <max.maroe@siliconbeachmedia.com>
2014-10-20 08:22:13 -0700

    Lightning ball

---
+++ Assets/13LightningBall/RandomTextureOffset.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RandomTextureOffset : MonoBehaviour 
+{
+	[System.Serializable]
+	public class MaterialInfo
+	{
+		public Material material;
+		public string textureName = "_MainTex";
+	}
+
+	public List<MaterialInfo> materialInfoList = new List<MaterialInfo>();
+
+	// Update is called once per frame
+	void Update ()
+	{
+		foreach(MaterialInfo info in materialInfoList)
+		{
+			info.material.SetTextureOffset(info.textureName,new Vector2(Random.value,Random.value));
+		}
+	}
+}
