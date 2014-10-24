c530304ce7d92d6a7ed557d8a923ef3fe2154153
Maximilian Maroe <max.maroe@siliconbeachmedia.com>
2014-10-23 20:07:11 -0700

    ZeroGAssist

---
+++ Assets/15ZeroGAssist/ActivateMonoBehaviourOnButton.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivateMonoBehaviourOnButton : MonoBehaviour
+{
+	public Behaviour[] componentsToActivate;
+	public string buttonName = "Fire1";
+
+	private bool componentsActive = false;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if(Input.GetButton(buttonName))
+		{
+			foreach(Behaviour com in componentsToActivate)
+			{
+				com.enabled = true;
+			}
+			componentsActive = true;
+		}
+		else if(componentsActive == true)
+		{
+			foreach(Behaviour com in componentsToActivate)
+			{
+				com.enabled = false;
+			}
+			componentsActive = false;
+		}
+	}
+}
