d3066ce0f94fc8be4c3520644fe27cca0c70584f
Maximilian Maroe <max.maroe@siliconbeachmedia.com>
2014-10-30 08:22:46 -0700

    Switching cameras and renaming a oflder

---
+++ Assets/19CameraSwitch/SwitchCamera.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchCamera : MonoBehaviour
+{
+	public Camera newCamera;
+	public string escapeKey = "Fire1";
+
+	private bool activeNow = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void Update()
+	{
+		if(activeNow)
+		{
+			if(Input.GetButtonDown(escapeKey))
+			{
+				GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>().enabled = true;
+				newCamera.enabled = false;
+				activeNow = false;
+			}
+		}
+	}
+
+	void Interact()
+	{
+		if(activeNow == false)
+		{
+			activeNow = true;
+			newCamera.enabled = true;
+			GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>().enabled = false;
+		}
+	}
+}
