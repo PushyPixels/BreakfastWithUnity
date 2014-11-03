60f0e38f94a163dad44ded2620f93182564b6948
Maximilian Maroe <drakfyre@gmail.com>
2014-11-03 08:23:20 -0800

    2d healthbar

---
+++ Assets/PlayerHealth.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour
+{
+	public Slider healthBar;
+	public int healthAmount = 10;
+	private int currentHealth;
+
+	// Use this for initialization
+	void Start ()
+	{
+		currentHealth = healthAmount;
+	}
+
+
+	
+	// Update is called once per frame
+	void OnTriggerEnter()
+	{
+		currentHealth--;
+		healthBar.value = (float)currentHealth/(float)healthAmount;
+	}
+}
