f4e42689a563b10c7817713c8751d32a13f33132
Maximilian Maroe <drakfyre@gmail.com>
2014-11-10 08:25:47 -0800

    Main reticle and a first pass on targeting

---
+++ Assets/26Reticles/TargetManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetManager : MonoBehaviour
+{
+	public Transform target;
+	public float scaleMultiplier = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		//transform.position = Camera.main.WorldToScreenPoint(target.position);
+		transform.localScale = target.renderer.bounds.size*scaleMultiplier;
+	}
+}
