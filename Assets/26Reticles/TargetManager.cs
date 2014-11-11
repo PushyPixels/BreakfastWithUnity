8bb788f4dad32da46b8415a8b9b5f40fb752bdac
Maximilian Maroe <drakfyre@gmail.com>
2014-11-11 08:23:00 -0800

    Better targeting

--- Assets/26Reticles/TargetManager.cs
+++ Assets/26Reticles/TargetManager.cs
@@ -3,18 +3,72 @@ using System.Collections;
 
 public class TargetManager : MonoBehaviour
 {
-	public Transform target;
 	public float scaleMultiplier = 1.0f;
+	public LayerMask targetMask = -1;
+
+	private RectTransform rectTransform;
+	private CanvasRenderer canvasRenderer;
+	private Transform target = null;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+		rectTransform = GetComponent<RectTransform>();
+		canvasRenderer = GetComponent<CanvasRenderer>();
+		//canvasRenderer.renderer.enabled = false;
+		transform.position = Vector3.one*10000.0f;
 	}
 	
 	// Update is called once per frame
-	void Update ()
+	void LateUpdate ()
 	{
-		//transform.position = Camera.main.WorldToScreenPoint(target.position);
-		transform.localScale = target.renderer.bounds.size*scaleMultiplier;
+		RaycastHit hit;
+		if(Physics.Raycast(Camera.main.transform.position,Camera.main.transform.forward,out hit,Mathf.Infinity,targetMask))
+		{
+			target = hit.transform;
+		}
+
+		if(target != null)
+		{
+			//canvasRenderer.renderer.enabled = true;
+			transform.position = Camera.main.WorldToScreenPoint(target.position);
+			Rect worldBounds = GUIRectWithObject(target);
+
+			rectTransform.sizeDelta = new Vector2(worldBounds.width,worldBounds.height)*scaleMultiplier;
+		}
+		else
+		{
+			transform.position = Vector3.one*10000.0f;
+			//canvasRenderer.renderer.enabled = false;
+		}
+	}
+
+	public static Rect GUIRectWithObject(Transform trans)
+	{
+		Vector3 cen = trans.renderer.bounds.center;
+		Vector3 ext = trans.renderer.bounds.extents;
+		Vector2[] extentPoints = new Vector2[8]
+		{
+			Camera.main.WorldToScreenPoint(new Vector3(cen.x-ext.x, cen.y-ext.y, cen.z-ext.z)),
+			Camera.main.WorldToScreenPoint(new Vector3(cen.x+ext.x, cen.y-ext.y, cen.z-ext.z)),
+			Camera.main.WorldToScreenPoint(new Vector3(cen.x-ext.x, cen.y-ext.y, cen.z+ext.z)),
+			Camera.main.WorldToScreenPoint(new Vector3(cen.x+ext.x, cen.y-ext.y, cen.z+ext.z)),
+			
+			Camera.main.WorldToScreenPoint(new Vector3(cen.x-ext.x, cen.y+ext.y, cen.z-ext.z)),
+			Camera.main.WorldToScreenPoint(new Vector3(cen.x+ext.x, cen.y+ext.y, cen.z-ext.z)),
+			Camera.main.WorldToScreenPoint(new Vector3(cen.x-ext.x, cen.y+ext.y, cen.z+ext.z)),
+			Camera.main.WorldToScreenPoint(new Vector3(cen.x+ext.x, cen.y+ext.y, cen.z+ext.z))
+		};
+		
+		Vector2 min = extentPoints[0];
+		Vector2 max = extentPoints[0];
+		
+		foreach(Vector2 v in extentPoints)
+		{
+			min = Vector2.Min(min, v);
+			max = Vector2.Max(max, v);
+		}
+		
+		return new Rect(min.x, min.y, max.x-min.x, max.y-min.y);
 	}
 }
