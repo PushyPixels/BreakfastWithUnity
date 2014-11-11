79eda88ded7a09d47c376a136befcf6b2efeafd3
Maximilian Maroe <drakfyre@gmail.com>
2014-11-11 14:33:49 -0800

    Some motion

---
+++ Assets/26Reticles/BrownianMotion.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BrownianMotion : MonoBehaviour
+{
+	public float speed = 1.0f;
+	public float turnDelay = 0.0f;
+	public float rotateSpeed = 90.0f;
+
+	private Quaternion goalRotation;
+
+	void Start()
+	{
+		goalRotation = Random.rotation;
+		Invoke ("Turn",turnDelay);
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.position += transform.forward*speed*Time.deltaTime;
+		transform.rotation = Quaternion.RotateTowards(transform.rotation,goalRotation,rotateSpeed*Time.deltaTime);
+	}
+
+	void Turn()
+	{
+		Invoke ("Turn",turnDelay);
+		goalRotation = Random.rotation;
+	}
+}
