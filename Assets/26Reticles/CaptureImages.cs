078dd72485973047875a28e544b1c38dfbcb0ca2
Maximilian Maroe <drakfyre@gmail.com>
2014-11-11 18:35:36 -0800

    Removing capture image from main camera

---
+++ Assets/26Reticles/CaptureImages.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CaptureImages : MonoBehaviour {
+
+	public int frameSkip = 3;
+
+
+	int i = 0;
+	int j = 0;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(j % frameSkip == 0)
+		{
+			Application.CaptureScreenshot("Screenshot" + i.ToString("D3") + ".png");
+			i++;
+		}
+		j++;
+	}
+}
