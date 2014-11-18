5f94a05c93acd0019f8997eb0fd3f745a72f2d10
Maximilian Maroe <drakfyre@gmail.com>
2014-11-18 10:27:48 -0800

    Ragdoll!

---
+++ Assets/30Ragdoll/DestroyAndDetachChildrenAtStart.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyAndDetachChildrenAtStart : MonoBehaviour
+{
+	// Use this for initialization
+	void Start ()
+	{
+		foreach(Transform child in transform)
+		{
+			child.parent = null;
+		}
+
+		Destroy(gameObject,1.0f);
+	}
+}
