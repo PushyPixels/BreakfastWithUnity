919e131881ce82a1578f27626e8f319aad9d7362
Maximilian Maroe <drakfyre@gmail.com>
2014-12-04 09:20:22 -0800

    Radial speedometer!

---
+++ Assets/38RadialSpeedometer/UIFillBasedOnVelocity.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIFillBasedOnVelocity : MonoBehaviour
+{
+	public Rigidbody objectToMeasure;
+	public float maxVelocity = 10.0f;
+
+	public bool disallowBackwards = false;
+
+	private Image image;
+
+	// Use this for initialization
+	void Start ()
+	{
+		image = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(!disallowBackwards || Vector3.Dot(objectToMeasure.velocity, objectToMeasure.transform.forward) > 0.0f)
+		{
+			image.fillAmount = objectToMeasure.velocity.magnitude/maxVelocity;
+		}
+		else
+		{
+			image.fillAmount = 0.0f;
+		}
+	}
+}
