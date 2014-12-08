c3d2992f7e6494dfd16949357dc30a97e87d4a7b
Maximilian Maroe <drakfyre@gmail.com>
2014-12-08 09:30:06 -0800

    Real time clock!

---
+++ Assets/39RealtimeClock/AnalogClock.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnalogClock : MonoBehaviour
+{
+	public Transform hourHand;
+	public Transform minuteHand;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		System.DateTime time = System.DateTime.Now;
+
+		Vector3 newRotation = hourHand.localEulerAngles;
+		newRotation.z = 360.0f/12.0f*time.Hour + 360.0f/12.0f/60.0f*time.Minute;
+		hourHand.localEulerAngles = newRotation;
+
+		newRotation = minuteHand.localEulerAngles;
+		newRotation.z = 360.0f/60.0f*time.Minute;
+		minuteHand.localEulerAngles = newRotation;
+	}
+}
---
+++ Assets/39RealtimeClock/DisplayTimeOnText.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DisplayTimeOnText : MonoBehaviour
+{
+	private Text clockText;
+
+	void Start()
+	{
+		clockText = GetComponent<Text>();
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		System.DateTime time = System.DateTime.Now;
+	
+		clockText.text = time.ToString("hh:mm");
+	}
+}
