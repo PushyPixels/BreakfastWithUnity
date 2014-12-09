74a2b74e952cc6734d269a9ab8646f8b859d877d
Maximilian Maroe <drakfyre@gmail.com>
2014-12-09 09:58:49 -0800

    Crossfade

---
+++ Assets/40AudioCrossfade/CrossfadeOnButton.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrossfadeOnButton : MonoBehaviour
+{
+	public AudioClip[] tracks;
+
+	public string buttonName = "Fire1";
+	public float fadeTime = 1.0f;
+
+	private int currentTrack = 0;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(Input.GetButtonDown(buttonName))
+		{
+			currentTrack++;
+			if(currentTrack >= tracks.Length)
+			{
+				currentTrack = 0;
+			}
+			MusicManager.Crossfade(tracks[currentTrack], fadeTime);
+		}
+	}
+}
---
+++ Assets/40AudioCrossfade/MusicManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour
+{
+	public static MusicManager Instance;
+	
+	void Awake ()
+	{
+		Instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public static void Crossfade(AudioClip newTrack, float fadeTime = 1.0f)
+	{
+		Instance.StopAllCoroutines();
+
+		if(Instance.GetComponents<AudioSource>().Length > 1)
+		{
+			Destroy(Instance.audio);
+		}
+
+		AudioSource newAudioSource = Instance.gameObject.AddComponent<AudioSource>();
+
+		newAudioSource.volume = 0.0f;
+
+		newAudioSource.clip = newTrack;
+
+		newAudioSource.Play();
+
+		Instance.StartCoroutine(Instance.ActuallyCrossfade(newAudioSource,fadeTime));
+	}
+
+	IEnumerator ActuallyCrossfade(AudioSource newSource, float fadeTime)
+	{
+		float t = 0.0f;
+
+		float initialVolume = audio.volume;
+
+		while(t < fadeTime)
+		{
+			audio.volume = Mathf.Lerp(initialVolume,0.0f,t/fadeTime);
+			newSource.volume = Mathf.Lerp(0.0f,1.0f,t/fadeTime);
+
+			t += Time.deltaTime;
+			yield return null;
+		}
+
+		newSource.volume = 1.0f;
+
+		Destroy(audio);
+	}
+
+
+}
