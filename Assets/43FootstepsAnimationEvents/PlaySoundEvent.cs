1b702cb8734ea9400df506888e5b9ad3edf1b2a8
Maximilian Maroe <drakfyre@gmail.com>
2014-12-15 09:23:30 -0800

    Animation Events!  Footsteps!

---
+++ Assets/43FootstepsAnimationEvents/PlaySoundEvent.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlaySoundEvent : MonoBehaviour
+{
+	void PlaySound()
+	{
+		audio.Play();
+	}
+}
