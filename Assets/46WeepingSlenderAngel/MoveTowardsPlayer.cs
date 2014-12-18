9416dc5ff42b6a3ec45433a66383b3d3f74665ea
Maximilian Maroe <drakfyre@gmail.com>
2014-12-18 09:29:57 -0800

    Weeping slender angel, and some change to footprints.unity for some reason I don't understand.

---
+++ Assets/46WeepingSlenderAngel/MoveTowardsPlayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTowardsPlayer : MonoBehaviour
+{
+	public float speed = 1.0f;
+
+	private GameObject player;
+
+	// Use this for initialization
+	void Start ()
+	{
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.position += (player.transform.position - transform.position).normalized*speed*Time.deltaTime;
+	}
+}
---
+++ Assets/46WeepingSlenderAngel/ToggleBehavioursOnInvisible.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleBehavioursOnInvisible : MonoBehaviour
+{
+	public Behaviour[] behavioursEnabledOnInvisible;
+	public Behaviour[] behavioursDisabledOnInvisible;
+	private bool visible = false;
+	private bool raycastable = false;
+
+	void Update()
+	{
+		RaycastHit hit;
+
+		if(Physics.Raycast(transform.position, Camera.main.transform.position - transform.position, out hit))
+		{
+			if(hit.collider.tag == "MainCamera")
+			{
+				raycastable = true;
+			}
+			else
+			{
+				raycastable = false;
+			}
+		}
+		else
+		{
+			raycastable = false;
+		}
+
+		if(raycastable && visible)
+		{
+			foreach(Behaviour bev in behavioursEnabledOnInvisible)
+			{
+				bev.enabled = false;
+			}
+			foreach(Behaviour bev in behavioursDisabledOnInvisible)
+			{
+				bev.enabled = true;
+			}
+		}
+		else
+		{
+			foreach(Behaviour bev in behavioursEnabledOnInvisible)
+			{
+				bev.enabled = true;
+			}
+			foreach(Behaviour bev in behavioursDisabledOnInvisible)
+			{
+				bev.enabled = false;
+			}
+		}
+	}
+
+	void OnBecameInvisible()
+	{
+		visible = false;
+	}
+
+	void OnBecameVisible()
+	{
+		visible = true;
+	}
+}
