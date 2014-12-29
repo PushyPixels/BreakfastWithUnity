cc48ea883dd5004602342a9fedf1d2cc5118a10b
Maximilian Maroe <drakfyre@gmail.com>
2014-12-29 09:18:58 -0800

    Trigger Doors

---
+++ Assets/47TriggerDoors/SetAnimationBoolOnTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetAnimationBoolOnTrigger : MonoBehaviour
+{
+	public string animationBoolName;
+	public Animator target;
+
+	void OnTriggerEnter()
+	{
+		target.SetBool(animationBoolName, true);
+	}
+
+	void OnTriggerExit()
+	{
+		target.SetBool(animationBoolName, false);
+	}
+}
