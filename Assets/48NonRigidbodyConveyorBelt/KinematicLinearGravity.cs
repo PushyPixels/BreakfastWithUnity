b923248524d88081dffb86e01818289745bc2bc3
Maximilian Maroe <drakfyre@gmail.com>
2014-12-30 09:33:29 -0800

    KinematicConveyorBelt

---
+++ Assets/48NonRigidbodyConveyorBelt/KinematicLinearGravity.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KinematicLinearGravity : MonoBehaviour
+{
+	public LayerMask layerMask = -1;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void LateUpdate ()
+	{
+		Vector3 gravityTime = Physics.gravity*Time.deltaTime;
+
+		RaycastHit hit;
+		if(Physics.Raycast(transform.position,Physics.gravity, out hit, gravityTime.magnitude,layerMask))
+		{
+			//Handle offset from surface problem here
+			//transform.position = hit.point;
+		}
+		else
+		{
+			transform.position += gravityTime;
+		}
+	}
+}
---
+++ Assets/48NonRigidbodyConveyorBelt/MoveTriggeredObjectForward.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTriggeredObjectForward : MonoBehaviour
+{
+	public float speed = 1.0f;
+
+	void OnTriggerStay(Collider col)
+	{
+		col.transform.position += transform.forward*speed*Time.deltaTime;
+	}
+}
