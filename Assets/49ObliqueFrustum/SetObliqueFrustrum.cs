fb0f1777ce42ed34fb3a464db8d0400bb35ba0ad
Maximilian Maroe <drakfyre@gmail.com>
2014-12-31 09:16:17 -0800

    Oblique camera frustrum!

---
+++ Assets/49ObliqueFrustum/SetObliqueFrustrum.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetObliqueFrustrum : MonoBehaviour
+{
+	public float horizontalOblique = 0.0f;
+	public float verticalOblique = 0.0f;
+
+	public bool setEveryFrame = false;
+
+	// Use this for initialization
+	void Start ()
+	{
+		SetObliqueness(horizontalOblique, verticalOblique);
+	}
+	
+	void Update ()
+	{
+		if(setEveryFrame)
+		{
+			SetObliqueness(horizontalOblique, verticalOblique);
+		}
+	}
+
+
+	void SetObliqueness(float horizObl, float vertObl)
+	{
+		Matrix4x4 mat = camera.projectionMatrix;
+		mat[0, 2] = horizObl;
+		mat[1, 2] = vertObl;
+		camera.projectionMatrix = mat;
+	}
+}
