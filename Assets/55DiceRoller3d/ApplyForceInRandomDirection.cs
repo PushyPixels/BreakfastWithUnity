b992d7942c735594e06bb21d0e99f7a50d5b7f73
Maximilian Maroe <drakfyre@gmail.com>
2015-01-16 09:23:38 -0800

    Dice roller part 1

---
+++ Assets/55DiceRoller3d/ApplyForceInRandomDirection.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ApplyForceInRandomDirection : MonoBehaviour
+{
+	public string buttonName = "Fire1";
+	public float forceAmount = 10.0f;
+	public float torqueAmount = 10.0f;
+	public ForceMode forceMode;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if(Input.GetButtonDown(buttonName))
+		{
+			rigidbody.AddForce(Random.onUnitSphere*forceAmount,forceMode);
+			rigidbody.AddTorque(Random.onUnitSphere*torqueAmount,forceMode);
+		}
+	}
+}
