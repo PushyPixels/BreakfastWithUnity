b37f21226652c5848bc537b8a3d2703e31b9ea4c
Maximilian Maroe <drakfyre@gmail.com>
2015-01-21 09:16:13 -0800

    Yay repo

---
+++ Assets/56DualStickShooterControls/MoveOnAxisInput.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveOnAxisInput : MonoBehaviour
+{
+	public string horizontalAxis = "Horizontal";
+	public string verticalAxis = "Vertical";
+	public float speed = 1.0f;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.position += (Vector3.right*Input.GetAxis(horizontalAxis) + Vector3.forward*Input.GetAxis(verticalAxis)).normalized*speed*Time.deltaTime;
+	}
+}
---
+++ Assets/56DualStickShooterControls/ShootOnAxisInput.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootOnAxisInput : MonoBehaviour
+{
+	public GameObject bullet;
+
+	public string horizontalAxis = "Horizontal";
+	public string verticalAxis = "Vertical";
+
+	public float shootDelay = 0.1f;
+
+	private bool canShoot = true;
+
+	
+	void ResetShot ()
+	{
+		canShoot = true;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Vector3 shootDirection = Vector3.right*Input.GetAxis(horizontalAxis) + Vector3.forward*Input.GetAxis(verticalAxis);
+		if(canShoot && shootDirection.sqrMagnitude > 0.0f)
+		{
+			transform.rotation = Quaternion.LookRotation(shootDirection,Vector3.up);
+			Instantiate(bullet,transform.position,transform.rotation);
+
+			canShoot = false;
+			Invoke("ResetShot",shootDelay);
+		}
+	}
+}
