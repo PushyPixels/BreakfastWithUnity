c0fb48723e616629754eddea70f0c6c9dab727b6
Maximilian Maroe <drakfyre@gmail.com>
2015-01-27 10:48:57 -0800

    Forgot to check in

---
+++ Assets/60BasicVisualizer/ChangeScaleBasedOnAudioVolume.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScaleBasedOnAudioVolume : MonoBehaviour
+{
+	public float scaleBoost = 1.0f;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		float[] samples = new float[1];
+		AudioListener.GetOutputData(samples,0);
+
+		float averageValue = 0.0f;
+		foreach(float sample in samples)
+		{
+			averageValue += sample;
+		}
+
+		averageValue = averageValue/samples.Length;
+
+		transform.localScale = Vector3.one*averageValue*scaleBoost;
+	}
+}
