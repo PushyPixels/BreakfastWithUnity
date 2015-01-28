1b2dd63419cd8f77127e873f9e81556583d514de
Maximilian Maroe <drakfyre@gmail.com>
2015-01-28 09:28:29 -0800

    Line visualizer

---
+++ Assets/61SpectrumVisualizer/LineVisualizer.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(LineRenderer))]
+public class LineVisualizer : MonoBehaviour
+{
+	public float size = 10.0f;
+	public float amplitude = 1.0f;
+	public int cutoffSample = 32; //MUST BE LOWER THAN SAMPLE SIZE
+	public FFTWindow fftWindow;
+	
+	private float[] samples = new float[64]; //MUST BE A POWER OF TWO
+	private LineRenderer lineRenderer;
+	private float stepSize;
+
+	void Start()
+	{
+		lineRenderer = GetComponent<LineRenderer>();
+		lineRenderer.SetVertexCount(cutoffSample);
+		stepSize = size/cutoffSample;
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		AudioListener.GetSpectrumData(samples,0,fftWindow);
+
+		int i = 0;
+
+		for(i = 0; i < cutoffSample; i++)
+		{
+			Vector3 position = new Vector3(i*stepSize - size/2.0f,samples[i]*amplitude,0.0f);
+			lineRenderer.SetPosition(i,position);
+		}
+	}
+}
