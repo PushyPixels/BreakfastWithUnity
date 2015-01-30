869e599eb57b4c7ff8f632c3ff03c660a538d184
Maximilian Maroe <drakfyre@gmail.com>
2015-01-30 09:18:35 -0800

    Line of sight fog of war

---
+++ Assets/62LineOfSightFogOfWar/LineOfSightRendering.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LineOfSightRendering : MonoBehaviour
+{
+	public string viewerTag = "Player";
+	public LayerMask layerMask = -1;
+
+	private GameObject player;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(player == null)
+		{
+			player = GameObject.FindGameObjectWithTag(viewerTag);
+		}
+
+		RaycastHit hit;
+
+		if(Physics.Raycast(transform.position,player.transform.position - transform.position,out hit,Mathf.Infinity,layerMask))
+		{
+			if(hit.collider.gameObject == player)
+			{
+				renderer.enabled = true;
+			}
+			else
+			{
+				renderer.enabled = false;
+			}
+		}
+		else
+		{
+			renderer.enabled = false;
+		}
+	
+	}
+}
