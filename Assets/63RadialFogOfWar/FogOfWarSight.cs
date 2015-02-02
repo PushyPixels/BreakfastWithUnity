0ed03e37ca6019e5287875cb5e21326335cc0175
Maximilian Maroe <drakfyre@gmail.com>
2015-02-02 09:17:48 -0800

    Radial fog-of-war part 1

---
+++ Assets/63RadialFogOfWar/FogOfWarSight.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FogOfWarSight : MonoBehaviour
+{
+	public float radius = 10.0f;
+	public LayerMask layerMask = -1;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		foreach(Collider col in Physics.OverlapSphere(transform.position,radius,layerMask))
+		{
+			col.SendMessage("Observed",SendMessageOptions.DontRequireReceiver);
+		}
+	}
+}
---
+++ Assets/63RadialFogOfWar/FogOfWarVisibility.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FogOfWarVisibility : MonoBehaviour
+{
+	private bool observed = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void Update()
+	{
+		if(observed)
+		{
+			renderer.enabled = true;
+		}
+		else
+		{
+			renderer.enabled = false;
+		}
+
+		observed = false;
+	}
+	
+	void Observed()
+	{
+		Debug.Log("Observed",gameObject);
+		observed = true;
+	}
+}
