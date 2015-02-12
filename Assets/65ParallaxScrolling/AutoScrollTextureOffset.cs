810c885d79f61a1af1cccb2c278bdc08e8fe43e1
Maximilian Maroe <drakfyre@gmail.com>
2015-02-12 09:18:39 -0800

    Parallax Part 1

---
+++ Assets/65ParallaxScrolling/AutoScrollTextureOffset.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoScrollTextureOffset : MonoBehaviour
+{
+	public Vector2 scrollSpeed = Vector2.one;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Vector2 finalTextureOffset = renderer.material.mainTextureOffset;
+		finalTextureOffset += scrollSpeed*Time.deltaTime;
+
+		while(finalTextureOffset.x >= 1.0f)
+		{
+			finalTextureOffset.x -= 1.0f;
+		}
+		while(finalTextureOffset.x < 0.0f)
+		{
+			finalTextureOffset.x += 1.0f;
+		}
+		while(finalTextureOffset.y >= 1.0f)
+		{
+			finalTextureOffset.y -= 1.0f;
+		}
+		while(finalTextureOffset.y < 0.0f)
+		{
+			finalTextureOffset.y += 1.0f;
+		}
+
+		renderer.material.mainTextureOffset = finalTextureOffset;
+	}
+}
