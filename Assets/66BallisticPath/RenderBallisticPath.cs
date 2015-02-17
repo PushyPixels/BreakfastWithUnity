15ced9ac8cd8c3035a1aa91afd5c8051ed552790
Maximilian Maroe <drakfyre@gmail.com>
2015-02-17 09:31:02 -0800

    Ballistic part 2

--- Assets/66BallisticPath/RenderBallisticPath.cs
+++ Assets/66BallisticPath/RenderBallisticPath.cs
@@ -3,9 +3,13 @@ using System.Collections;
 
 public class RenderBallisticPath : MonoBehaviour
 {
+	public GameObject explosionDisplay;
 	public float initialVelocity = 10.0f;
 	public float timeResolution = 0.02f;
 	public float maxTime = 10.0f;
+	public LayerMask layerMask = -1;
+
+	private GameObject explosionDisplayInstance;
 
 	private LineRenderer lineRenderer;
 
@@ -29,6 +33,39 @@ public class RenderBallisticPath : MonoBehaviour
 		for(float t = 0.0f; t < maxTime; t += timeResolution)
 		{
 			lineRenderer.SetPosition(index,currentPosition);
+
+			RaycastHit hit;
+
+			if(Physics.Raycast(currentPosition,velocityVector,out hit,velocityVector.magnitude*timeResolution,layerMask))
+			{
+				lineRenderer.SetVertexCount(index+2);
+
+				lineRenderer.SetPosition(index+1,hit.point);
+
+				if(explosionDisplay != null)
+				{
+					if(explosionDisplayInstance != null)
+					{
+						explosionDisplayInstance.SetActive(true);
+						explosionDisplayInstance.transform.position = hit.point;
+					}
+					else
+					{
+						explosionDisplayInstance = Instantiate(explosionDisplay,hit.point,Quaternion.identity) as GameObject;
+						explosionDisplayInstance.SetActive(true);
+					}
+				}
+
+				break;
+			}
+			else
+			{
+				if(explosionDisplayInstance != null)
+				{
+					explosionDisplayInstance.SetActive(false);
+				}
+			}
+
 			currentPosition += velocityVector*timeResolution;
 			velocityVector += Physics.gravity*timeResolution;
 			index++;
