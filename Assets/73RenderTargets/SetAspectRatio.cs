9791af6741420d456c0b816afe8c6159285ca143
Maximilian Maroe <drakfyre@gmail.com>
2015-03-10 09:25:11 -0700

    Render targets!

---
+++ Assets/73RenderTargets/SetAspectRatio.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetAspectRatio : MonoBehaviour
+{
+	public float aspectRatio = 1.0f;
+	public bool realtimeUpdate = false;
+
+	private Camera camera;
+
+	// Use this for initialization
+	void Start ()
+	{
+		camera = GetComponent<Camera>();
+	}
+
+	void Update()
+	{
+		if(realtimeUpdate)
+		{
+			camera.aspect = aspectRatio;
+		}
+	}
+}
---
+++ Assets/73RenderTargets/SetAspectRatioBasedOnCamera.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetAspectRatioBasedOnCamera : MonoBehaviour
+{
+	public Camera target;
+
+	// Use this for initialization
+	void Start ()
+	{
+		GetComponent<Camera>().aspect = target.aspect;
+	}
+}
