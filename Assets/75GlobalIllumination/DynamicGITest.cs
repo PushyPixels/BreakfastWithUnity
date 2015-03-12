ae6ed72f91b609a43b022b21c644f96e8952b716
Maximilian Maroe <drakfyre@gmail.com>
2015-03-12 12:27:35 -0700

    Playing around

---
+++ Assets/75GlobalIllumination/DynamicGITest.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DynamicGITest : MonoBehaviour
+{
+	public float hue = 0.0f;
+	public float saturation = 1.0f;
+	public float brightness = 1.0f;
+
+	private Renderer renderer;
+	private Color color;
+
+	// Use this for initialization
+	void Start ()
+	{
+		renderer = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		color = new HSBColor(hue,saturation,brightness).ToColor(); 
+		renderer.sharedMaterial.SetColor("_Emission",color);
+		DynamicGI.SetEmissive(renderer, color);
+	}
+}
