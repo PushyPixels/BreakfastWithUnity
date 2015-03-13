70a3b69d28e99ef48979314f4bee5541d9e3067b
Maximilian Maroe <drakfyre@gmail.com>
2015-03-13 08:56:15 -0700

    Music test files

---
+++ Assets/75GlobalIllumination/Reload.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Reload : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () {
+		Invoke ("ReloadNow", 5.0f);
+	}
+	
+	// Update is called once per frame
+	void ReloadNow () {
+		Application.LoadLevel(Application.loadedLevel);
+	}
+}
