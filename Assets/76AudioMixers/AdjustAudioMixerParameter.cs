2d7fada1060b1bede834ff822069c1ec7a704fb5
Maximilian Maroe <drakfyre@gmail.com>
2015-03-16 09:26:19 -0700

    Mixers and FX and funtimes yay!

---
+++ Assets/76AudioMixers/AdjustAudioMixerParameter.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using System.Collections;
+
+public class AdjustAudioMixerParameter : MonoBehaviour
+{
+	public AudioMixer mixer;
+	public float value; //This is mostly for testing
+	public string parameterName;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public void SetAudioParameter(float newValue)
+	{
+		value = newValue;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		mixer.SetFloat(parameterName,value);
+	}
+}
