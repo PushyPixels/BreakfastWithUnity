dabc21b038229824955af337a2bf8ff77ac04763
Maximilian Maroe <drakfyre@gmail.com>
2015-03-19 10:38:48 -0700

    Show prep

---
+++ Assets/78PixelPerfect/PixelPerfectScale.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PixelPerfectScale : MonoBehaviour
+{
+	public int screenVerticalPixels = 256;
+
+	public bool preferUncropped = true;
+
+	private float screenPixelsY = 0;
+	
+	private bool currentCropped = false;
+
+	void Update()
+	{
+		if(screenPixelsY != (float)Screen.height || currentCropped != preferUncropped)
+		{
+			screenPixelsY = (float)Screen.height;
+			currentCropped = preferUncropped;
+
+			float screenRatio = screenPixelsY/screenVerticalPixels;
+			float ratio;
+
+			if(preferUncropped)
+			{
+				ratio = Mathf.Floor(screenRatio)/screenRatio;
+			}
+			else
+			{
+				ratio = Mathf.Ceil(screenRatio)/screenRatio;
+			}
+
+			transform.localScale = Vector3.one*ratio;
+		}
+	}
+}
