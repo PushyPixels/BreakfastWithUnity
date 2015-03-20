178d00e51006b9f10b30d5bdc0fbb039a731b537
Maximilian Maroe <drakfyre@gmail.com>
2015-03-20 09:10:56 -0700

    Code rot and fixes for dice roller

---
+++ Assets/55DiceRoller3d/SetMaxAngularVelocity.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetMaxAngularVelocity : MonoBehaviour
+{
+	public float maxAngularVelocity = 7.0f;
+
+	// Use this for initialization
+	void Start ()
+	{
+		GetComponent<Rigidbody>().maxAngularVelocity = maxAngularVelocity;
+	}
+}
