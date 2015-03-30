21c7aaf932cbdfa47a5879ab390022ede9944b07
Maximilian Maroe <drakfyre@gmail.com>
2015-03-30 09:13:54 -0700

    Scriptable objects episode!

---
+++ Assets/80ScriptableObjects/ScriptableObjectReferenceExample.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptableObjectReferenceExample : MonoBehaviour
+{
+	public TestScriptableObject scriptableObjectReference;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Debug.Log(scriptableObjectReference.exampleFloat);
+		Instantiate(scriptableObjectReference.gameObjectReference,scriptableObjectReference.exampleVector,Quaternion.identity);
+	}
+}
---
+++ Assets/80ScriptableObjects/TestScriptableObject.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestScriptableObject : ScriptableObject
+{
+	public float exampleFloat;
+	public Vector3 exampleVector;
+	public GameObject gameObjectReference;
+}
