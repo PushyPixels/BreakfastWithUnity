9e218de1519f689230d5757d2aaa45219f785701
Maximilian Maroe <drakfyre@gmail.com>
2015-04-07 08:54:54 -0700

    Visualizer funtimes

---
+++ Assets/82SpawningBasedOnAudio/SpawnBasedOnAudio.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnBasedOnAudio : MonoBehaviour
+{
+	public GameObject objectPrefab;
+	public float spawnThreshold = 0.5f;
+	public int frequency = 0;
+	public FFTWindow fftWindow;
+	//public float debugValue;
+	
+	private float[] samples = new float[1024]; //MUST BE A POWER OF TWO
+	
+	void Start()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		AudioListener.GetSpectrumData(samples,0,fftWindow);
+
+		//debugValue = samples[frequency];
+
+		if(samples[frequency] > spawnThreshold)
+		{
+			Instantiate(objectPrefab,transform.position,transform.rotation);
+		}
+	}
+}
