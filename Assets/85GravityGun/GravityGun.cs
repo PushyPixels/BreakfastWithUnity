c192ac4fff1d3b1bd2ad6023a9b92a8ac17b9736
Maximilian Maroe <drakfyre@gmail.com>
2015-04-16 09:24:21 -0700

    Gravity gun and explosion stuff

---
+++ Assets/85GravityGun/GravityGun.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityGun : MonoBehaviour
+{
+	public string fireButton = "Fire1";
+	public float grabDistance = 10.0f;
+	public Transform holdPosition;
+	public float throwForce = 100.0f;
+	public ForceMode throwForceMode;
+	public LayerMask layerMask = -1;
+
+	private GameObject heldObject = null;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(heldObject == null)
+		{
+			if(Input.GetButtonDown(fireButton))
+			{
+				RaycastHit hit;
+				if(Physics.Raycast(transform.position,transform.forward,out hit,grabDistance,layerMask))
+				{
+					heldObject = hit.collider.gameObject;
+					heldObject.GetComponent<Rigidbody>().isKinematic = true;
+					heldObject.GetComponent<Collider>().enabled = false;
+				}
+			}
+		}
+		else
+		{
+			heldObject.transform.position = holdPosition.position;
+			heldObject.transform.rotation = holdPosition.rotation;
+
+			if(Input.GetButtonDown(fireButton))
+			{
+				Rigidbody body = heldObject.GetComponent<Rigidbody>();
+				body.isKinematic = false;
+				heldObject.GetComponent<Collider>().enabled = true;
+				body.AddForce(throwForce*transform.forward,throwForceMode);
+				heldObject = null;
+			}
+		}
+	}
+}
---
+++ Assets/86BlockableExplosions/AddBlockableExplosionForceOnStart.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddBlockableExplosionForceOnStart : MonoBehaviour
+{
+	public float force = 100.0f;
+	public float radius = 5.0f;
+	public float upwardsModifier = 0.0f;
+	public ForceMode forceMode;
+
+	// Use this for initialization
+	void Start ()
+	{
+		foreach(Collider col in Physics.OverlapSphere(transform.position, radius))
+		{
+			if(col.GetComponent<Rigidbody>() != null)
+			{
+				RaycastHit hit;
+				if(Physics.Raycast(transform.position,col.transform.position-transform.position,out hit,Mathf.Infinity))
+				{
+					if(hit.collider == col)
+					{
+						col.GetComponent<Rigidbody>().AddExplosionForce(force,transform.position,radius,upwardsModifier,forceMode);
+					}
+				}
+			}
+		}
+	}
+}
