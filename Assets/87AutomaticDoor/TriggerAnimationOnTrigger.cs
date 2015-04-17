68404dcb81606dcf43a2a5e598eb785faf37474b
Maximilian Maroe <drakfyre@gmail.com>
2015-04-17 09:25:34 -0700

    Automatic door

---
+++ Assets/87AutomaticDoor/TriggerAnimationOnTrigger.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerAnimationOnTrigger : MonoBehaviour
+{
+	public Animator animator;
+	public string onTriggerEnterParameterName;
+	public string onTriggerExitParameterName;
+
+	void Start()
+	{
+		if(animator == null)
+		{
+			animator = GetComponent<Animator>();
+			if(animator == null)
+			{
+				Debug.LogError("No animator component on this script!",gameObject);
+			}
+		}
+	}
+
+	void OnTriggerEnter()
+	{
+		if(onTriggerEnterParameterName != null)
+		{
+			animator.SetTrigger(onTriggerEnterParameterName);
+		}
+	}
+
+	void OnTriggerExit()
+	{
+		if(onTriggerExitParameterName != null)
+		{
+			animator.SetTrigger(onTriggerExitParameterName);
+		}
+	}
+}
