dd2367cddabeca30bb27a2f17e517a1ea90e1c1d
Maximilian Maroe <drakfyre@gmail.com>
2015-05-05 09:23:38 -0700

    Raycast Gun!

---
+++ Assets/91RaycastGun/RaycastGun.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunHit
+{
+	public float damage;
+	public RaycastHit raycastHit;
+}
+
+public class RaycastGun : MonoBehaviour
+{
+	public float fireDelay = 0.1f;
+	public float damage = 1.0f;
+	public string buttonName = "Fire1";
+	public LayerMask layerMask = -1;
+
+	private bool readyToFire = true;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(Input.GetButtonDown(buttonName) && readyToFire)
+		{
+			RaycastHit hit;
+			if(Physics.Raycast(transform.position,transform.forward,out hit,Mathf.Infinity,layerMask))
+			{
+				GunHit gunHit = new GunHit();
+				gunHit.damage = damage;
+				gunHit.raycastHit = hit;
+				hit.collider.SendMessage("Damage",gunHit,SendMessageOptions.DontRequireReceiver);
+			}
+		}
+	}
+}
---
+++ Assets/91RaycastGun/SpawnOnDamage.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnOnDamage : MonoBehaviour
+{
+	public GameObject objectToSpawn;
+
+	void Damage(GunHit gunHit)
+	{
+		Instantiate(objectToSpawn,gunHit.raycastHit.point,Quaternion.LookRotation(gunHit.raycastHit.normal));
+	}
+}
