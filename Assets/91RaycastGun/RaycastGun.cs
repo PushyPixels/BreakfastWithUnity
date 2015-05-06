b5ecbb33eb2cd1b51f05f88584b0e451b9206c48
Maximilian Maroe <drakfyre@gmail.com>
2015-05-06 09:21:58 -0700

    Gun spread!

--- Assets/91RaycastGun/RaycastGun.cs
+++ Assets/91RaycastGun/RaycastGun.cs
@@ -12,9 +12,12 @@ public class RaycastGun : MonoBehaviour
 	public float fireDelay = 0.1f;
 	public float damage = 1.0f;
 	public string buttonName = "Fire1";
+	public float maxBulletSpreadAngle = 15.0f;
+	public float timeTillMaxSpreadAngle = 1.0f;
 	public LayerMask layerMask = -1;
 
 	private bool readyToFire = true;
+	private float fireTime;
 
 	// Use this for initialization
 	void Start ()
@@ -25,16 +28,43 @@ public class RaycastGun : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		if(Input.GetButtonDown(buttonName) && readyToFire)
+		if(Input.GetButton(buttonName)) //GetButtonDown for semi-auto, GetButton for automatic fire
 		{
-			RaycastHit hit;
-			if(Physics.Raycast(transform.position,transform.forward,out hit,Mathf.Infinity,layerMask))
+			fireTime += Time.deltaTime;
+
+			if(readyToFire)
 			{
-				GunHit gunHit = new GunHit();
-				gunHit.damage = damage;
-				gunHit.raycastHit = hit;
-				hit.collider.SendMessage("Damage",gunHit,SendMessageOptions.DontRequireReceiver);
+				RaycastHit hit;
+
+				Vector3 fireDirection = transform.forward;
+
+				Quaternion fireRotation = Quaternion.LookRotation(fireDirection);
+
+				Quaternion randomRotation = Random.rotation;
+
+				float currentSpread = Mathf.Lerp(0.0f, maxBulletSpreadAngle, fireTime/timeTillMaxSpreadAngle);
+
+				fireRotation = Quaternion.RotateTowards(fireRotation,randomRotation,Random.Range(0.0f,currentSpread));
+
+				if(Physics.Raycast(transform.position,fireRotation*Vector3.forward,out hit,Mathf.Infinity,layerMask))
+				{
+					GunHit gunHit = new GunHit();
+					gunHit.damage = damage;
+					gunHit.raycastHit = hit;
+					hit.collider.SendMessage("Damage",gunHit,SendMessageOptions.DontRequireReceiver);
+					readyToFire = false;
+					Invoke("SetReadyToFire", fireDelay);
+				}
 			}
 		}
+		else
+		{
+			fireTime = 0.0f;
+		}
+	}
+
+	void SetReadyToFire()
+	{
+		readyToFire = true;
 	}
 }
