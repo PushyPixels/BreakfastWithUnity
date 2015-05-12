0333d4a39ea92c3f296d335da640d02330a1697d
Maximilian Maroe <drakfyre@gmail.com>
2015-05-12 09:29:13 -0700

    Shotgun!

---
+++ Assets/92ShotgunInheritance/ShotgunWeapon.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShotgunWeapon : WeaponBase
+{
+	public int shotFragments = 8;
+	public float spreadAngle = 10.0f;
+
+	protected override void PrimaryFire()
+	{
+		for(int i = 0; i < shotFragments; i++)
+		{
+			RaycastHit hit;
+			
+			Quaternion fireRotation = Quaternion.LookRotation(transform.forward);
+			
+			Quaternion randomRotation = Random.rotation;
+			
+			fireRotation = Quaternion.RotateTowards(fireRotation,randomRotation,Random.Range(0.0f,spreadAngle));
+
+			if(Physics.Raycast(transform.position,fireRotation*Vector3.forward,out hit,Mathf.Infinity,layerMask))
+			{
+				GunHit gunHit = new GunHit();
+				gunHit.damage = damage;
+				gunHit.raycastHit = hit;
+				hit.collider.SendMessage("Damage",gunHit,SendMessageOptions.DontRequireReceiver);
+			}
+		}
+	}
+}
---
+++ Assets/92ShotgunInheritance/WeaponBase.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class WeaponBase : MonoBehaviour
+{
+	public float damage = 1.0f;
+	public int maxAmmo = 0; //0 for infinite ammo
+	public float fireDelay = 0.1f;
+	public string primaryFire = "Fire1";
+	public LayerMask layerMask = -1;
+	public bool automaticFire = false;
+
+	private bool readyToFire = true;
+	private int currentAmmo;
+
+	protected abstract void PrimaryFire();
+
+	void Start()
+	{
+		currentAmmo = maxAmmo;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		CheckInput();
+	}
+
+	protected virtual void CheckInput()
+	{
+		bool primaryFirePressed;
+		
+		if(automaticFire)
+		{
+			primaryFirePressed = Input.GetButton(primaryFire);
+		}
+		else
+		{
+			primaryFirePressed = Input.GetButtonDown(primaryFire);
+		}
+		
+		if(primaryFirePressed) //GetButtonDown for semi-auto, GetButton for automatic fire
+		{	
+			if(readyToFire && (currentAmmo > 0 || maxAmmo == 0))
+			{
+				PrimaryFire();
+				readyToFire = false;
+				currentAmmo--;
+				Invoke("SetReadyToFire", fireDelay);
+			}
+		}
+	}
+
+	void SetReadyToFire()
+	{
+		readyToFire = true;
+	}
+}
