468f46cfd3e178437839468fce95c6aaae3516d5
Maximilian Maroe <drakfyre@gmail.com>
2015-06-22 11:15:27 -0700

    Some fun stuff for the rag doll ep, now loops without breaking your computer

---
+++ Assets/30Ragdoll/DestroyOnInvisible.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyOnInvisible : MonoBehaviour
+{
+	public GameObject objectToDestroy;
+
+	void Start()
+	{
+		if(!objectToDestroy)
+		{
+			objectToDestroy = gameObject;
+		}
+	}
+
+	void OnBecameInvisible()
+	{
+		Destroy (objectToDestroy);
+	}
+}
