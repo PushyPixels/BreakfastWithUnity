e3a2a26e4c2c600df81346cd98b4526e90b7a2f7
Maximilian Maroe <drakfyre@gmail.com>
2015-07-23 10:40:23 -0700

    More tree action!

--- Assets/93RecursionTree/RecursionTree.cs
+++ Assets/93RecursionTree/RecursionTree.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class RecursionTree : MonoBehaviour
 {
@@ -9,6 +10,18 @@ public class RecursionTree : MonoBehaviour
 	public float spreadAngle = 30.0f;
 	public float iterations = 3;
 	public float splits = 3;
+	public float windAngle = 10.0f;
+	public float minWindChangeTime = 1.0f;
+	public float maxWindChangeTime = 5.0f;
+
+	private List<Transform> childList = new List<Transform>();
+	private bool doneParenting = false;
+	private Quaternion originalRotation;
+
+	[HideInInspector]
+	public bool root = true;
+
+	private static Quaternion goalRotation;
 
 
 	// Use this for initialization
@@ -24,13 +37,51 @@ public class RecursionTree : MonoBehaviour
 
 				RecursionTree instance = Instantiate(twigReference,transform.position+transform.forward*twigLength,twigRotation) as RecursionTree;
 				instance.iterations = iterations - 1;
+				instance.root = false;
+
+				childList.Add(instance.transform);
 			}
 		}
+
+		originalRotation = transform.rotation;
+
+		if(root)
+		{
+			WindChange();
+			Invoke("WindChange",Random.Range(minWindChangeTime,maxWindChangeTime));
+		}
 	}
-	
+
+	void WindChange()
+	{
+		goalRotation = Random.rotation;
+		Invoke("WindChange",Random.Range(minWindChangeTime,maxWindChangeTime));
+	}
+
 	// Update is called once per frame
 	void Update ()
 	{
-	
+		if(!doneParenting)
+		{
+			foreach(Transform child in childList)
+			{
+				child.parent = transform;
+			}
+			doneParenting = true;
+		}
+		else
+		{
+			//Quaternion randomRotation = Random.rotation;
+
+			Quaternion maxRotation = Quaternion.RotateTowards(originalRotation,goalRotation,windAngle);
+
+			transform.rotation = Quaternion.RotateTowards(transform.rotation,maxRotation,Random.Range(0.0f,windAngle*Time.deltaTime));
+		}
+
+
+		if(root)
+		{
+			Debug.DrawRay(transform.position,goalRotation*transform.forward);
+		}
 	}
 }
